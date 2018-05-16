70fdc2b015ffa3a2078202f7218a168027dbda5d
flowness <yocohen@gmail.com>
2018-05-16 10:44:19 +0300

    Alerts Configuration UI - ext

--- app/src/main/java/com/flowness/activities/AlertsConfigActivity.java
+++ app/src/main/java/com/flowness/activities/AlertsConfigActivity.java
@@ -1,55 +1,42 @@
 package com.flowness.activities;
 
-import android.app.DatePickerDialog;
+import android.app.AlertDialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.widget.DatePicker;
-import android.widget.EditText;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.flowness.R;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.flowness.utils.SharedPreferencesKeys;
 
 import java.util.Calendar;
 
-public class AlertsConfigActivity extends AppCompatActivity implements View.OnClickListener{
-
+public class AlertsConfigActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+    Switch swFreezeAlert;
+    Switch swIrregularityAlert;
+    Switch swLeakageAlert;
+    Switch swZeroFlowAlert;
+    Switch swMonthlyCostAlert;
     Calendar calZeroFlowStart = Calendar.getInstance();
-    TextView etZeroFlowStart;
+    TextView tvZeroFlowStart;
     TimePickerDialog.OnTimeSetListener tplStart;
     Calendar calZeroFlowEnd = Calendar.getInstance();
-    TextView etZeroFlowEnd;
+    TextView tvZeroFlowEnd;
     TimePickerDialog.OnTimeSetListener tplEnd;
-
-    private class myTimeSetListener implements TimePickerDialog.OnTimeSetListener {
-        TextView tv;
-        Calendar calendar;
-        public myTimeSetListener(TextView tv, Calendar calendar) {
-            this.tv = tv;
-            this.calendar = calendar;
-        }
-
-        @Override
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            calendar.set(Calendar.HOUR, hourOfDay);
-            calendar.set(Calendar.MINUTE,minute);
-            String hourMinute = String.format("%s%d:%s%d", (hourOfDay >= 10 ? "" : "0"), hourOfDay, (minute >= 10 ? "" : "0"), minute);
-            tv.setText(hourMinute);
-        }
-    }
-
+    TextView tvMonthlyCost;
+    TextView tvMonthlyCostUnits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,14 +45,36 @@ public class AlertsConfigActivity extends AppCompatActivity implements View.OnCl
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        etZeroFlowStart = findViewById(R.id.zero_flow_alert_start);
-        etZeroFlowStart.setOnClickListener(this);
-        tplStart = new myTimeSetListener(etZeroFlowStart, calZeroFlowStart);
-        etZeroFlowEnd = findViewById(R.id.zero_flow_alert_end);
-        etZeroFlowEnd.setOnClickListener(this);
-        tplEnd = new myTimeSetListener(etZeroFlowEnd, calZeroFlowEnd);
+        tvZeroFlowStart = findViewById(R.id.zero_flow_alert_start);
+        tvZeroFlowStart.setOnClickListener(this);
+        tplStart = new myTimeSetListener(tvZeroFlowStart, calZeroFlowStart);
+        tvZeroFlowEnd = findViewById(R.id.zero_flow_alert_end);
+        tvZeroFlowEnd.setOnClickListener(this);
+        tplEnd = new myTimeSetListener(tvZeroFlowEnd, calZeroFlowEnd);
+        tvMonthlyCost = findViewById(R.id.monthly_cost_alert_amount);
+        tvMonthlyCost.setOnClickListener(this);
+        swFreezeAlert = findViewById(R.id.freeze_alert_switch);
+        swFreezeAlert.setOnCheckedChangeListener(this);
+        swIrregularityAlert = findViewById(R.id.irregularity_alert_switch);
+        swIrregularityAlert.setOnCheckedChangeListener(this);
+        swLeakageAlert = findViewById(R.id.leakage_alert_switch);
+        swLeakageAlert.setOnCheckedChangeListener(this);
+        swZeroFlowAlert = findViewById(R.id.zero_flow_alert_switch);
+        swZeroFlowAlert.setOnCheckedChangeListener(this);
+        swMonthlyCostAlert = findViewById(R.id.monthly_cost_alert_switch);
+        swMonthlyCostAlert.setOnCheckedChangeListener(this);
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(SharedPreferencesKeys.SP_ROOT_NAME, MODE_PRIVATE); // 0 - for private mode
+        tvMonthlyCostUnits = findViewById(R.id.monthly_cost_alert_unit);
+        tvMonthlyCostUnits.setText(pref.getInt(SharedPreferencesKeys.METER_UNITS_PREF_KEY, 0) == 0 ? "Liters" : "Gallons");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setValuesFromDB();
+    }
+
+    private void setValuesFromDB() {
+        
     }
 
     @Override
@@ -73,9 +82,115 @@ public class AlertsConfigActivity extends AppCompatActivity implements View.OnCl
 
 //                DatePicker datePicker = new DatePicker();
 //                datePicker.show(getFragmentManager(), "Select date");
-        if (v == etZeroFlowStart || v == etZeroFlowEnd) {
-            TimePickerDialog timePickerDialog = new TimePickerDialog(AlertsConfigActivity.this, v == etZeroFlowStart ? tplStart : tplEnd, 0, 0, true);
+        if (v == tvZeroFlowStart || v == tvZeroFlowEnd) {
+            TimePickerDialog timePickerDialog = new TimePickerDialog(AlertsConfigActivity.this, v == tvZeroFlowStart ? tplStart : tplEnd, 0, 0, true);
             timePickerDialog.show();
+        } else if (v == tvMonthlyCost) {
+            RelativeLayout linearLayout = new RelativeLayout(AlertsConfigActivity.this);
+            final NumberPicker numberPicker = new NumberPicker(AlertsConfigActivity.this);
+            numberPicker.setMaxValue(100000);
+            numberPicker.setMinValue(100);
+            numberPicker.setValue(Integer.valueOf((String) tvMonthlyCost.getText()));
+
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(50, 50);
+            RelativeLayout.LayoutParams numPickerParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            numPickerParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+            linearLayout.setLayoutParams(params);
+            linearLayout.addView(numberPicker, numPickerParams);
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(AlertsConfigActivity.this);
+            alertDialogBuilder.setTitle("Select the number");
+            alertDialogBuilder.setView(linearLayout);
+            alertDialogBuilder.setCancelable(false);
+            alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Log.e("AlertsConfig", String.format("New Quantity Value : %d", numberPicker.getValue()));
+                    tvMonthlyCost.setText(String.format("%d", numberPicker.getValue()));
+                }
+            });
+            alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getTag().toString()) {
+            case "swfreeze": {
+                if (isChecked) {
+                    Toast.makeText(AlertsConfigActivity.this,
+                            "Switch On", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(AlertsConfigActivity.this,
+                            "Switch Off", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+            case "swirregularity": {
+                if (isChecked) {
+                    Toast.makeText(AlertsConfigActivity.this,
+                            "Switch On", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(AlertsConfigActivity.this,
+                            "Switch Off", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+            case "swleakage": {
+                if (isChecked) {
+                    Toast.makeText(AlertsConfigActivity.this,
+                            "Switch On", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(AlertsConfigActivity.this,
+                            "Switch Off", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+            case "swzeroflow": {
+                setEnabledLinearLayout(isChecked, R.id.zero_flow_alert_hours_layout);
+                break;
+            }
+            case "swmonthly": {
+                setEnabledLinearLayout(isChecked, R.id.monthly_cost_alert_config_amount_layout);
+                break;
+            }
+        }
+    }
+
+    private void setEnabledLinearLayout(boolean isChecked, int id) {
+        LinearLayout layout = findViewById(id);
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            View child = layout.getChildAt(i);
+            child.setEnabled(isChecked);
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    private class myTimeSetListener implements TimePickerDialog.OnTimeSetListener {
+        TextView tv;
+        Calendar calendar;
+
+        myTimeSetListener(TextView tv, Calendar calendar) {
+            this.tv = tv;
+            this.calendar = calendar;
+        }
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            calendar.set(Calendar.HOUR, hourOfDay);
+            calendar.set(Calendar.MINUTE, minute);
+            String hourMinute = String.format("%s%d:%s%d", (hourOfDay >= 10 ? "" : "0"), hourOfDay, (minute >= 10 ? "" : "0"), minute);
+            tv.setText(hourMinute);
         }
     }
 
